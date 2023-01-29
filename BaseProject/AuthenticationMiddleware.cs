b85b8eeb94e427acb5bce9c8c38ec30586674eda
muammerKrc <muammer03karaca@gmail.com>
2023-01-29 20:50:32 +0300

    added identity to base project

---
+++ BaseProject/AuthenticationMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject
+{
+    public static class  AuthenticationRegistration
+    {
+        public static void AddAuthenticationRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppIdentityDbContext>(i =>
+            {
+                i.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddIdentity<AppUser,IdentityRole>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 5;
+                opt.Password.RequiredUniqueChars = 3;
+            }).AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+        }
+    }
+}
--- BaseProject/Program.cs
+++ BaseProject/Program.cs
@@ -1,9 +1,19 @@
+using BaseProject;
+using BaseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using static System.Formats.Asn1.AsnWriter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthenticationRegistration(builder.Configuration);
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,11 +27,30 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+using var scope = app.Services.CreateScope();
+
+var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+identityDbContext.Database.Migrate();
+
+
+if (!userManager.Users.Any())
+{
+    userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@outlook.com" }, "Password12*").Wait();
+
+    userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@outlook.com" }, "Password12*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@outlook.com" }, "Password12*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@outlook.com" }, "Password12*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@outlook.com" }, "Password12*").Wait();
+}
+
 
 app.Run();
